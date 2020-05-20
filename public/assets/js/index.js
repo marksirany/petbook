a652422b29879235803033daf232efba8bb11443
Mark Sirany <marksirany@me.com>
2020-05-20 03:51:10 -0500

    trying sometihng

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,5 @@
 const $noteTitle = $(".note-title");
-const $noteText = $(".note-textarea");
+const $noteText = $(".note-date");
 const $saveNoteBtn = $(".save-note");
 const $newNoteBtn = $(".new-note");
 const $noteList = $(".list-container .list-group");
